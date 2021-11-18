278a5ce93bf436704afba6fbd7bcc55b7de79c56
changmok <hong1071@nate.com>
2021-11-18 15:28:18 +0900

    model

--- mysite/controllers/user.js
+++ mysite/controllers/user.js
@@ -1,6 +1,13 @@
 module.exports = {
+    joinsuccess: function(req, res){
+        res.render('user/joinsuccess');
+    },
     join: function(req, res){
         //res.local.applicationName = '...';          // view의 ejs에서 applicationName을 사용하는 방법!!!! 중요함!!
         res.render('user/join');
+    },
+    _join: function(req, res){
+        console.log(req.body);
+        res.redirect('/user/joinsuccess');
     }
 }
--- mysite/routes/user.js
+++ mysite/routes/user.js
@@ -2,5 +2,7 @@ const express = require('express');
 const controller = require('../controllers/user');
 const router = express.Router();
 router.route('/join').get(controller.join);
+router.route('/join').post(controller._join);
+router.route('/joinsuccess').get(controller.joinsuccess);
 
 module.exports = router;
